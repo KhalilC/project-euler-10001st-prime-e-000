caa77383e48793eef4f0242f7bcbde9a57588152
KhalilC <klcrumpler@gmail.com>
2015-10-20 02:07:21 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+require 'prime'
+
+def prime_number_for(number)
+  counter = 0
+  first_prime = 2
+  loop do
+    counter += 1 if first_prime.prime?
+    break if counter == number
+    first_prime += 1
+  end
+  first_prime
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  attr_accessor :nth_number
+  def initialize(nth_number)
+    @nth_number = nth_number
+  end
+
+  def number
+    counter = 0
+    first_prime = 2
+    loop do
+      counter += 1 if first_prime.prime?
+      break if counter == nth_number
+      first_prime += 1
+    end
+    first_prime
+  end
+end
